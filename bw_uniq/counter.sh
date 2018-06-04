b8335653a92c5ab1ccc11ee370251cf1144211ec
WilliamBarela <william.barela.dev@gmail.com>
2018-06-04 17:57:29 -0500

    split file name to be used for awk statement

--- bw_uniq/counter.sh
+++ bw_uniq/counter.sh
@@ -8,6 +8,12 @@
 companies_uniq_csv=$1
 journal_year=$2
 
+journal_year_extension="${journal_year##*.}"
+journal_year_string="${journal_year%.*}"
+
+journal=$(echo $journal_year_string | sed -E -e 's/(.*)_(.*)/\1/')
+year=$(echo $journal_year_string | sed -E -e 's/(.*)_(.*)/\2/')
+
 declare -a companies
 let i=0
 
@@ -18,3 +24,9 @@ while (( ${#companies[@]} > i )); do
   cat $journal_year | grep -iow "${companies[i]}" | uniq -c; 
   ((++i)); 
 done; i=0 
+
+
+echo "journal: $journal"
+echo "year: $year"
+
+# echo $journal_year_string
