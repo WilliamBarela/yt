3a6217e714ad8b00a2c488bcd880c222db295ce0
WilliamBarela <william.barela.dev@gmail.com>
2018-06-04 17:24:51 -0500

    standardized argvs of counter.sh

--- bw_uniq/counter.sh
+++ bw_uniq/counter.sh
@@ -1,14 +1,17 @@
 ################################################################################
 #                                                                              #
 #                                                                              #
-#           ./counter.sh journal_uniq_csv journal_year                         #
+#           ./counter.sh companies_uniq_csv journal_year                       #
 #                                                                              #
 #                                                                              #
 ################################################################################
-declare -a arr
+companies_uniq_csv=$1
+journal_year=$2
+
+declare -a companies
 let i=0
 
 #while IFS=$'\n' read -r line_data; do arr[i]="${line_data}"; ((++i)); done < companies_uniq.csv
-readarray -t arr < companies_uniq.csv
+readarray -t companies < $companies_uniq_csv
 
-while (( ${#arr[@]} > i )); do cat business_week_year_abstract.csv | grep -iow "${arr[i]}" | uniq -c; ((++i)); done; i=0 
+while (( ${#companies[@]} > i )); do cat $journal_year | grep -iow "${companies[i]}" | uniq -c; ((++i)); done; i=0 
