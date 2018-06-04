d147801098dfe4e8476d72d5409ef9a801da635a
WilliamBarela <william.barela.dev@gmail.com>
2018-06-04 18:42:52 -0500

    finished formatting function

--- bw_uniq/counter.sh
+++ bw_uniq/counter.sh
@@ -14,6 +14,15 @@ journal_year_string="${journal_year%.*}"
 journal=$(echo $journal_year_string | sed -E -e 's/(.*)_(.*)/\1/')
 year=$(echo $journal_year_string | sed -E -e 's/(.*)_(.*)/\2/')
 
+function format_to_csv () {
+
+  #echo "$1";
+  count_wo_spaces=$(echo "$1" | sed -E -e 's/\s+/,/g'); #| awk -F, '{print $2" : " $3}');
+  # echo "$count_wo_spaces" # > $journal"_"count.csv
+  count=$(echo "$count_wo_spaces" | awk -F, '{print $2","$3}' OFS=",");
+  echo "$journal,$year,$count" >> $journal"_"count.csv;
+}
+
 declare -a companies
 let i=0
 
@@ -22,7 +31,7 @@ readarray -t companies < $companies_uniq_csv
 
 while (( ${#companies[@]} > i )); do 
   company_count=$(cat $journal_year | grep -iow "${companies[i]}" | uniq -c);
-  [[ ! -z "$company_count" ]] && echo $company_count;
+  [[ ! -z "$company_count" ]] && format_to_csv "$company_count";
   ((++i)); 
 done; i=0 
 
